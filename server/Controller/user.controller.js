6351134d3b744aa47dd37f49519979c7d29034ac
oluwaseun Adeyeri <adeyeriseun0@gmail.com>
2024-06-07 01:47:17 +0100

    saving information to mysql successfully

--- server/Controller/user.controller.js
+++ server/Controller/user.controller.js
@@ -1,11 +1,43 @@
-const db = require('../model/user.model');
+const db = require("../model/user.model");
+const bcrypt = require("bcryptjs");
+const signup = (req, res) => {
+  
+   const {
+     callUpNo,
+     firstName,
+     lastName,
+     middleName,
+     telephoneNo,
+     email,
+     password,
+   } = req.body;
 
-const signup = (req, res)=>{
-//    const {callup_num, firstName, lastName, middleName, tel, email, password} = req.body;
-console.log(req.body);
 
-}
+   bcrypt.hash(password, 10)
+   .then((hashedPassword) => {      
+       const query = "INSERT INTO safetyiq_table (callUp_num, firstName, lastName, middleName, tel, email, password) VALUES (?, ?, ?, ?, ?, ?, ?)";
+       const values = [
+         callUpNo,
+         firstName,
+         lastName,
+         middleName,
+         telephoneNo,
+         email,
+         hashedPassword,
+       ];
+     
+       db.query(query, values, (err, results) => {
+         if (err) {
+           console.error("Error inserting user:", err);
+           res.status(500).send("Internal Server Error");
+           return;
+         }
+         res.send("User registered successfully");
+       });
+   })
+ 
+};
 
 module.exports = {
-    signup,
-}
+  signup,
+};
