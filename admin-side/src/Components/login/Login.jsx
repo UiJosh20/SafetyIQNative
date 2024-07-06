5582469d2129c1c4324c94084aad00c211f85b73
Oluwaseun Adeyeri <adeyeriseun0@gmail.com>
2024-07-06 23:17:31 +0100

    created admin login page

--- admin-side/src/Components/login/Login.jsx
+++ admin-side/src/Components/login/Login.jsx
@@ -2,12 +2,34 @@ import React from 'react'
 
 const Login = () => {
   return (
-
     <>
-    <h1>login</h1>
-      
+      <section className="bg-gray-200 h-screen w-screen flex flex-col justify-center items-center">
+        <form className="card bg-white shadow-lg bg-base-100 rounded-lg p-14 border border-solid w96">
+          <input
+            type="text"
+            placeholder="Email"
+            className="outline-none ms-10 w-96 my-5"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className=" outline-none w-96 ms-10 my-10"
+          />
+
+          <button className="btn bg-red-500 mb-3 mt-5 p-3 rounded-md text-white font-bold w-full">
+            Login
+          </button>
+          <a href="#" className='text-gray-500 text-sm text-center  my-10'>Don't have an account? Sign up Forgot password?</a>
+          <p className="text-center my-6">OR</p>
+    <div className='w-full flex justify-center'>
+          <button className="btn bg-gray-800 mt-5 p-3 rounded-md text-white font-bold w-72">
+            Create Account
+          </button>
+    </div>
+        </form>
+      </section>
     </>
-  )
+  );
 }
 
 
