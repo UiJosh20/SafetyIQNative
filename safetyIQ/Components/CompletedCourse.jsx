4e2a056e74ab1215ba72465ebb3cd27e68b1ebab
Oluwaseun Adeyeri <adeyeriseun0@gmail.com>
2024-09-10 14:55:06 +0100

    update

--- safetyIQ/Components/CompletedCourse.jsx
+++ safetyIQ/Components/CompletedCourse.jsx
@@ -1,14 +1,26 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const CompletedCourse = () => {
   return (
-    <View>
-      <Text>CompletedCourse</Text>
-    </View>
-  )
+    <SafeAreaView>
+      <Text style={{ fontSize: 30, fontWeight: "bold" }}>CompletedCourse</Text>
+
+      <View style={styles.container}>
+        <Text> No completed course</Text>
+      </View>
+    </SafeAreaView>
+  );
 }
 
 export default CompletedCourse
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent:"center",
+        alignItems:"center"
+    }
+
+})
